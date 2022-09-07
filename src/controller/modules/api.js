fc567a25ef6cc6b7d437a6b919b1ec360766b065
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-09-07 16:00:24 +0800

    feat: update api admin

--- src/controller/modules/api.js
+++ src/controller/modules/api.js
@@ -7,18 +7,16 @@ const test_api = async (ctx, next) => {
 }
 
 const post_api = async (ctx, next) => {
-  ctx.rest({
-    body: ctx.request.body
-  })
+  ctx.rest(ctx.request.body)
 }
 
 const add_user = async (ctx, next) => {
-  ctx.rest({
-    body: ctx.request.body
-  })
+  console.log(ctx.request.body)
+  ctx.rest({ msg: '添加成功', user: ctx.request.body })
 }
 
 export default {
   'GET /api/test_api': test_api,
-  'POST /api/post_api': post_api
+  'POST /api/post_api': post_api,
+  'POST /api/add_user': add_user,
 }
